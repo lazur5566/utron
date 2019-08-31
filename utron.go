29b83c51b0b0b6a4a6e9cacac6cbb180da9be859
Afon <yifeng@17.media>
2019-08-31 19:40:43 +0800

    update

--- utron.go
+++ utron.go
@@ -1,15 +1,19 @@
 package utron
 
-import "github.com/lazur5566/utron/app"
+import (
+	"io"
+
+	"github.com/lazur5566/utron/app"
+)
 
 // NewApp creates a new bare-bone utron application. To use the MVC components, you should call
 // the Init method before serving requests.
-func NewApp() *app.App {
-	return app.NewApp()
+func NewApp(logOut io.Writer) *app.App {
+	return app.NewApp(logOut)
 }
 
 // NewMVC creates a new MVC utron app. If cfg is passed, it should be a directory to look for
 // the configuration files. The App returned is initialized.
-func NewMVC(cfg ...string) (*app.App, error) {
-	return app.NewMVC(cfg...)
+func NewMVC(logOut io.Writer, cfg ...string) (*app.App, error) {
+	return app.NewMVC(logOut, cfg...)
 }
